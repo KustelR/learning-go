96cf900b396b7afa3ebf5d467ebd7e93f666b66e
KustelR <dmitriysstudios2@gmail.com>
2023-11-15 22:39:23 +0400

    Unused swap logic deleted

--- src/linkedList/node.go
+++ src/linkedList/node.go
@@ -22,46 +22,4 @@ func (n *node) swap(n2 *node) {
 	nValue := n.value
 	n.value = n2.value
 	n2.value = nValue
-	/*
-		nNext := n.next
-		nPrev := n.prev
-		n2Next := n2.next
-		n2Prev := n2.prev
-		if *nNext.prev == *n2Prev {
-			n2.next = n2Prev
-			n2.prev = nPrev
-			n.prev = nNext
-			n.next = n2Next
-			return
-		}
-		if nPrev != nil && n2Next != nil && *nPrev == *n2Next {
-			n2.prev = n2Next
-			n2.next = nNext
-			n.next = nPrev
-			n.prev = n2Prev
-			return
-		}
-		if n2Next != nil {
-			n2Next.prev = n
-		}
-		if n2Prev != nil {
-			n2Prev.next = n
-		}
-		if nNext != nil {
-			nNext.prev = n2
-		}
-		if nPrev != nil {
-			nPrev.next = n2
-		}
-		n.next = n2Next
-		n.prev = n2Prev
-		n2.next = nNext
-		n2.prev = nPrev
-	*/
 }
-
-/*
-func (n node) String() string {
-	return fmt.Sprintf("[DLL node] value: %v, next: %v prev: %v", n.value, n.next, n.prev)
-}
-*/
