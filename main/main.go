926bcbf814ec342c96e4b3ccbed9f4fed5065f2c
KustelR <dmitriysstudios2@gmail.com>
2023-11-15 22:39:57 +0400

    websocket tests

--- main/main.go
+++ main/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	wstrying "wsTrying"
 )
 
 func main() {
 	fmt.Println("Hello")
+	server := wstrying.StartServer(HandleMessage)
+	var command string
+	for {
+		fmt.Scanln(&command)
+		if command == "shutdown" {
+			break
+		}
+		switch command {
+		case "lookup":
+			{
+				fmt.Println(server)
+			}
+		}
+	}
+}
+
+func HandleMessage(message wstrying.Message) {
+	fmt.Println(string(message.Msg))
+	message.Session.SendMessage(message.Mt, message.Msg)
 }
